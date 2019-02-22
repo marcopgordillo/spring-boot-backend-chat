e2bb6cfb87c57d91249d053b40c4c9125adc7297
marcopgordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-02-22 17:00:09 -0500

    Dando un color al username cuando se conecta al servidor

--- src/main/java/com/example/chat/controllers/ChatController.java
+++ src/main/java/com/example/chat/controllers/ChatController.java
@@ -6,9 +6,12 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import java.util.Date;
+import java.util.Random;
 
 @Controller
 public class ChatController {
+
+    private String[] colores = {"red", "green", "blue", "magenta", "purple", "orange"};
 
     @MessageMapping("/mensaje")
     @SendTo("/chat/mensaje")
@@ -17,6 +20,7 @@ public class ChatController {
         mensaje.setFecha(new Date().getTime());
 
         if (mensaje.getTipo().equals("NUEVO_USUARIO")) {
+            mensaje.setColor(colores[new Random().nextInt(colores.length)]);
             mensaje.setTexto("Nuevo Usuario");
         }
 
